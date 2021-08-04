47e65594d669b192ffdd77cb9c0ffb03a0a31cc0
BravoClassic <gerald.akorli@gmail.com>
2021-08-04 18:52:32 +0000

    Added motivational quotes

--- dev-friend/notifications.js
+++ dev-friend/notifications.js
@@ -1,11 +1,12 @@
 const vscode = require('vscode');
+const motivate = require('inspirational-quotes');
 
 const hydratedTask = () =>  {
-    vscode.window.showInformationMessage('Drink some water and get hydrated!');
+    vscode.window.showInformationMessage(`Drink some water and get hydrated!\n ${motivate.getRandomQuote().text} \n -${motivate.getRandomQuote().author}`);
 }
 
 const restTask = () =>  {
-    vscode.window.showInformationMessage('Have A Break And Take Rest!');
+    vscode.window.showInformationMessage(`Have A Break And Take Rest!\n ${motivate.getRandomQuote().text} \n -${motivate.getRandomQuote().author}`);
 }
 
 module.exports = { hydratedTask, restTask};
