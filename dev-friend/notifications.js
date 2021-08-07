ccdc9f52d407dc4c32368ddcaf755f7e9b463bf1
BravoClassic <gerald.akorli@gmail.com>
2021-08-07 21:21:26 +0000

    Fixed inspirational quotes error

--- dev-friend/notifications.js
+++ dev-friend/notifications.js
@@ -2,11 +2,13 @@ const vscode = require('vscode');
 const motivate = require('inspirational-quotes');
 
 const hydratedTask = () =>  {
-    vscode.window.showInformationMessage(`Drink some water and get hydrated!\n ${motivate.getRandomQuote().text} \n -${motivate.getRandomQuote().author}`);
+    vscode.window.showInformationMessage(`Drink some water and get hydrated!`);
+    vscode.window.showInformationMessage(`${motivate.getRandomQuote()}`);
 }
 
 const restTask = () =>  {
-    vscode.window.showInformationMessage(`Have A Break And Take Rest!\n ${motivate.getRandomQuote().text} \n -${motivate.getRandomQuote().author}`);
+    vscode.window.showInformationMessage(`Have A Break And Take Rest!`);
+    vscode.window.showInformationMessage(`${motivate.getRandomQuote()}`);
 }
 
 module.exports = { hydratedTask, restTask};
